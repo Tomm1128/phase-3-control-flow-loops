b9a2196cb43302d8ce35ad5e98700d84292941cc
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-07-08 15:24:03 -0400

    Practice with loops in Ruby

--- looping.rb
+++ looping.rb
@@ -1,5 +1,15 @@
+require "pry"
+
 def happy_new_year
   # your code here
+  counter = 10
+  while counter > 0
+      puts counter
+      if counter == 1
+        puts "Happy New Year!"
+      end
+      counter -= 1
+  end
 end
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
@@ -17,8 +27,20 @@ end
 
 def fizzbuzz_printer
   # your code here
+  counter = 1
+  until counter == 101
+    puts fizzbuzz(counter)
+    counter += 1
+  end
 end
 
 def reverse_string(str)
   # your code here
+  newStr = []
+  str.length.times do |i|
+    newStr.unshift(str[i])
+  end
+  newStr.join
 end
+
+reverse_string("Hello")
